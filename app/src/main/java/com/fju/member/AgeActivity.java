caf2768a5c69a4cf3f2b2398381d3ddcb3e32d47
miia <miia@com.tw>
2020-01-07 15:05:47 +0800

    2st

--- app/src/main/java/com/fju/member/AgeActivity.java
+++ app/src/main/java/com/fju/member/AgeActivity.java
@@ -2,13 +2,24 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AgeActivity extends AppCompatActivity {
+
+    private EditText edAge;
+    private ImageButton next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        edAge = findViewById(R.id.age);
+        next = findViewById(R.id.next3);
+        Intent intent =new Intent(this,GenderActivity.class);
     }
+
 }
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -2,13 +2,25 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NicknameActivity extends AppCompatActivity {
+
+    private EditText edName;
+    private ImageButton next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+        edName = findViewById(R.id.nickname);
+        next = findViewById(R.id.next3);
+        Intent intent =new Intent(this,AgeActivity.class);
+
     }
 }
+
