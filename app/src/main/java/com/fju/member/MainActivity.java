239e8c79b2fee434fa4e0fcc5d59960aa4f35fa9
miia <miia@com.tw>
2020-01-07 14:11:36 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,6 +2,7 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //期末考試
+        Intent intent = new Intent(this,NicknameActivity.class);
+        startActivity(intent);
     }
 }
